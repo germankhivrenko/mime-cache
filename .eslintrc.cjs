727fa61284821fc3c8a7420e135af7e04479f62a
Herman Khivrenko <germankhivrenko@gmail.com>
2022-08-25 10:14:11 +0300

    style: added eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  'env': {
+    'es6': true,
+    'node': true,
+    'mocha': true, // can be applied as a file comment: /* eslint-env mocha */ 
+  },
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'extends': [
+    'eslint:recommended'
+  ],
+  'rules': {
+  }
+};
+
